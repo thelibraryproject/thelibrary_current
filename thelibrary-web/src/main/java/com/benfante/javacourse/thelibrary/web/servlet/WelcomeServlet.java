5a67db75054f3ae3a3601c6ed08115ee8941e36e
Lucio Benfante <lucio@benfante.com>
2019-11-11 16:37:56 +0100

    Moved to servlets 4.0

---
+++ thelibrary-web/src/main/java/com/benfante/javacourse/thelibrary/web/servlet/WelcomeServlet.java
@@ -0,0 +1,44 @@
+package com.benfante.javacourse.thelibrary.web.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Date today = new Date();
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/welcome.jsp");
+		request.setAttribute("todayDate", today);
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
